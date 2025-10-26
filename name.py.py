d4cafe27ecdbdd60b86fd84c8a65ca8f92680b27
vajaypersaud77777-blip <vajay.persaud77777@gmail.com>
2025-10-26 08:32:33 -0700

    Add files via upload

---
+++ name.py.py
@@ -0,0 +1,3 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Vajay Persaud")
